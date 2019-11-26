d10a6c256064794aa891027dfe4d8b38bbaca892
jharte5 <jonathan.harte@codeimmersives.com>
2019-11-26 16:02:09 -0500

    almost

---
+++ unmarkLi.js
@@ -0,0 +1,3 @@
+// const markLi = function () {
+//     document.querySelectorAll('.complete').forEach(item => {item.addEventListener('click', function () {item.classList.remove('complete')})});
+// }
